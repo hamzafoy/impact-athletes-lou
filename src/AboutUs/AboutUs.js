20629b6552be04125278369f6931a4bcecc80872
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-03 05:16:25 -0400

    add a basic about-us content section in the About Us container.

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -5,22 +5,19 @@ import './AboutUs.css';
 function AboutUs() {
     return (
       <div className="about-us-container">
-        <header className="App-header">
         
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-  
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-  
-        </header>
+        <section className="about-us">
+
+            <h1 className="about-us__heading">
+                About Us
+            </h1>
+
+            <p className="about-us__text">
+                We provide a full service shop to meet the philanthropic goals of Athletes and Coaches of all levels. We help athletes make an IMPACT in their community by tapping into their natural leadership abilities to serve their communities. Our staff has over 10 years being involved in athletics and the non-profit world. Whether you want to have a single day event, or a legacy program our staff can help you build your dreams of IMPACT.
+            </p>
+
+        </section>
+
       </div>
     );
   }
