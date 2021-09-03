caf0fbfefc87bf5d09d176e6a3bda7e482a9dad0
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-03 05:20:03 -0400

    add an additional section which will contain info on staff.

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -13,7 +13,29 @@ function AboutUs() {
             </h1>
 
             <p className="about-us__text">
-                We provide a full service shop to meet the philanthropic goals of Athletes and Coaches of all levels. We help athletes make an IMPACT in their community by tapping into their natural leadership abilities to serve their communities. Our staff has over 10 years being involved in athletics and the non-profit world. Whether you want to have a single day event, or a legacy program our staff can help you build your dreams of IMPACT.
+                We provide a full service shop to meet the philanthropic goals of Athletes and 
+                Coaches of all levels. We help athletes make an IMPACT in their community by 
+                tapping into their natural leadership abilities to serve their communities. 
+                Our staff has over 10 years of involvement in athletics and the non-profit world. 
+                Whether you want to have a single day event or a legacy program, our staff can help 
+                you build your dreams of IMPACT.
+            </p>
+
+        </section>
+
+        <section className="about-us">
+
+            <h1 className="about-us__heading">
+                About Us
+            </h1>
+
+            <p className="about-us__text">
+                We provide a full service shop to meet the philanthropic goals of Athletes and 
+                Coaches of all levels. We help athletes make an IMPACT in their community by 
+                tapping into their natural leadership abilities to serve their communities. 
+                Our staff has over 10 years of involvement in athletics and the non-profit world. 
+                Whether you want to have a single day event or a legacy program, our staff can help 
+                you build your dreams of IMPACT.
             </p>
 
         </section>
