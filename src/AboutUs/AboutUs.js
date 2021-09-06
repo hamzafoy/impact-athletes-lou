e30e620718db64f9b15d964176c85ecb05817b13
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-05 22:12:15 -0400

    start work on staff section and upload staff headshot fotos.

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -23,20 +23,40 @@ function AboutUs() {
 
         </section>
 
-        <section className="about-us">
+        <section className="staff-container">
 
-            <h1 className="about-us__heading">
-                About Us
-            </h1>
+            <aside className="staff">
 
-            <p className="about-us__text">
-                We provide a full service shop to meet the philanthropic goals of Athletes and 
-                Coaches of all levels. We help athletes make an IMPACT in their community by 
-                tapping into their natural leadership abilities to serve their communities. 
-                Our staff has over 10 years of involvement in athletics and the non-profit world. 
-                Whether you want to have a single day event or a legacy program, our staff can help 
-                you build your dreams of IMPACT.
-            </p>
+                <h1 className="staff__heading">
+                    Ricky Santiago
+                </h1>
+
+                <p className="staff__text">
+
+                </p>
+
+            </aside>
+
+            <aside className="staff">
+
+                <h1 className="staff__heading">
+                    Isai Sanchez
+                </h1>
+
+                <p className="staff__text">
+                Isai “Izzy” Sanchez was born in Ciudad Cordoba, Veracruz, Mexico. 
+                In his home town, his father, Isai Sanchez Sr., played for the 
+                professional division II soccer team Los Tiburones Rojos de Veracruz. 
+                Izzy’s grandfather, Luis Miguel Sanchez, was also a professional 
+                Luchador and boxer in Mexico. Izzy played High School football at 
+                Louisville Central High School where he primarily played Left Tackle. 
+                Izzy is currently training to be a power lifter and boxer. Izzy has a long 
+                history in the non-profit world and minority outreach. Izzy currently 
+                runs the division of Latino/Spanish Outreach for the first Black Owned 
+                Title company in Louisville. 
+                </p>
+
+            </aside>
 
         </section>
 
