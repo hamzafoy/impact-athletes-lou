ee52d1a20d52462ad0ff8ca2ad45dbbaf53656ee
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-07 04:00:43 -0400

    trying to improve landing page media queries further.

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -7,8 +7,13 @@ function LandingPage() {
   return (
     <div className="App">
       
-      <img className="stock-image" src={stockimage} alt="Stock foto of a RB trying to juke the defense." />
+        <h2 className="mission-text"><span>To capture the natural leadership qualities of athletes for the betterment of their community.</span></h2>
 
+        <img className="stock-image" src={stockimage} alt="Stock foto of a RB trying to juke the defense."/>
+        
+        <h2 className="vision-text"><span>A community of Athletes that impact their communities with their services.</span></h2>
+      
+      
       <section className="title-and-svg">
 
         <h2 className="name-text">Impact Athletes</h2>
@@ -32,10 +37,6 @@ function LandingPage() {
         </svg>
 
       </section>
-
-      <h2 className="mission-text"><span>To capture the natural leadership qualities of athletes for the betterment of their community.</span></h2>
-
-      <h2 className="vision-text"><span>A community of Athletes that impact their communities with their services.</span></h2>
 
     </div>
   );
