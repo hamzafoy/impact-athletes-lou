e1335ba732f1ca407db867b34f1ef8e62ff96dcf
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-21 05:45:01 -0400

    begin work on the testimonies section.

--- src/Works/Works.js
+++ src/Works/Works.js
@@ -10,6 +10,18 @@ class Works extends React.Component {
         return(
             <div className="works-container">
 
+                <blockquote className="works__quote">
+                    <p>
+                    Dear Mr. Santiago and Mr. Clemente:
+                    We hope that this letter finds you and your loved ones healthy and well.
+                    On behalf of our organization, Puerto Rico Rise Up, I wanted to thank you for your
+                    recent collaboration with us, as we work for the well-being of the children of Puerto
+                    Rico. Our organization wishes to continue and strengthen these types of
+                    collaborations with our community. Seeing the faces of joy and hope are what fuels
+                    PRRU to continue working.
+                    </p>
+                </blockquote>
+
             </div>
         )
 
