8cd8ee139d79cdeb880b6f422b098fe0073b918a
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-29 14:21:36 -0400

    update staff section per client's request.

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -59,83 +59,46 @@ function AboutUs() {
 
             </aside>
 
-            {/* <div className="staff">
+            <aside className="staff-card">
 
-                <div className="staff_card front">
+                <figure className="staff-card-picframe">
 
-                    <img className="staff_card__foto" alt="Ricky Santiago's headshot" src={rsantiago}/>
+                    <img className="staff-card__img" alt="Ricky Santiago's headshot" src={isanchez}/>
 
-                    <h1 className="staff__heading">
-                        Ricky Santiago
-                    </h1>
-
-                </div>
-
-                <div className="staff_card back">
-
-                    <p className="staff__text">
-                        Our CEO and Founder Ricky Santiago grew up in a very Sports
-                        Oriented family. Originally from Ponce, Puerto Rico, a town that takes
-                        sports very seriously that&#39;s the birth place of World Class athletes, by
-                        way of Louisville, KY another town that takes sports very serious, its
-                        was inevitable to be influenced by sports. Having played high school
-                        sport and coaching for HS sports for more than 6 years, Ricky has a
-                        depth of understanding about the mindset of Athletes. Ricky has
-                        served on multiple non-profit boards in the city of Louisville. Ricky
-                        also has a Master’s in Organizational Leadership from Indiana Tech
-                        University.
-                    </p>
-
-                </div>
-
-            </div>
-
-            <div className="staff">
-
-                <div className="staff_card front">
-
-                    <img className="staff_card__foto" alt="Isai Sanchez's headshot" src={isanchez}/>
-
-                    <h1 className="staff__heading">
+                    <figcaption className="staff-card__piclabel">
                         Isai Sanchez
-                    </h1>
+                    </figcaption>
 
-                </div>
-                
-                <div className="staff_card back">
+                </figure>
 
-                    <p className="staff__text">
-                        Isai “Izzy” Sanchez was born in Ciudad Cordoba, Veracruz, Mexico. 
-                        In his home town, his father, Isai Sanchez Sr., played for the 
-                        professional division II soccer team Los Tiburones Rojos de Veracruz. 
-                        Izzy’s grandfather, Luis Miguel Sanchez, was also a professional 
-                        Luchador and boxer in Mexico. Izzy played High School football at 
-                        Louisville Central High School where he primarily played Left Tackle. 
-                        Izzy is currently training to be a power lifter and boxer. Izzy has a long 
-                        history in the non-profit world and minority outreach. Izzy currently 
-                        runs the division of Latino/Spanish Outreach for the first Black Owned 
-                        Title company in Louisville.
-                    </p>
+                <p className="staff-card__text">
+                    Isai “Izzy” Sanchez was born in Ciudad Cordoba, Veracruz, Mexico. 
+                    In his home town, his father, Isai Sanchez Sr., played for the 
+                    professional division II soccer team Los Tiburones Rojos de Veracruz. 
+                    Izzy’s grandfather, Luis Miguel Sanchez, was also a professional 
+                    Luchador and boxer in Mexico. Izzy played High School football at 
+                    Louisville Central High School where he primarily played Left Tackle. 
+                    Izzy is currently training to be a power lifter and boxer. Izzy has a long 
+                    history in the non-profit world and minority outreach. Izzy currently 
+                    runs the division of Latino/Spanish Outreach for the first Black Owned 
+                    Title company in Louisville.
+                </p>
 
-                </div>
+            </aside>
 
-            </div>
+            <aside className="staff-card">
 
-            <div className="staff">
+                <figure className="staff-card-picframe">
 
-                <div className="staff_card front">
+                    <img className="staff-card__img" alt="Ricky Santiago's headshot" src={jreyes}/>
 
-                    <img className="staff_card__foto" alt="Juliana Reyes' headshot" src={jreyes}/>
-
-                    <h1 className="staff__heading">
+                    <figcaption className="staff-card__piclabel">
                         Juliana Reyes
-                    </h1>
+                    </figcaption>
 
-                </div>
-                
-                <div className="staff_card back">
+                </figure>
 
-                    <p className="staff__text">
+                <p className="staff-card__text">
                     With a focus on biology for her bachelor’s, Juliana Reyes was born in Ponce, Puerto Rico 
                     with a passion for helping people. During her high school time Juliana tried and succeeded 
                     in a vast majority of school activities with a focus on sports and fitness. Her time as a 
@@ -143,12 +106,10 @@ function AboutUs() {
                     with losses and to be gracious in victory, something that has helped her in her professional 
                     life. After graduating she focused on sciences and pursued a career in the medical field. 
                     During her studies in Dominican Republic, she participated in various community services 
-                    helping the less fortunate communities with medical care. 
-                    </p>
+                    helping the less fortunate communities with medical care.
+                </p>
 
-                </div>
-
-            </div> */}
+            </aside>
 
         </section>
 
